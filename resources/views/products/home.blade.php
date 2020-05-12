eb154e404b0220678d6525bb1e782b0ae7244552
Mukti Wibowo <muktiw7@gmail.com>
2020-05-12 21:03:17 +0700

    Small adjustments

--- resources/views/products/home.blade.php
+++ resources/views/products/home.blade.php
@@ -3,10 +3,10 @@
 @section('default-content')
 <div class="row">
     <div class="container-fluid cover-image"></div>
-    <div class="col-12 text-center py-5">
+    <div class="col-12 text-center py-4">
         <ul class="nav justify-content-center">
             <li class="nav-item mx-2">
-                <a class="cb cb-outline" href="#">VIEW CATALOG</a>
+                <a class="cb cb-outline" href="{{route('catalog')}}">VIEW CATALOG</a>
             </li>
         </ul>
     </div>
